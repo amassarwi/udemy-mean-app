6fccd80d3254388facec84d336b60a44842f10b2
Amir Massarwa <amassarwi@gmail.com>
2019-05-12 01:16:01 +0200

    Added mongoose and connected to local db to add posts

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/udemy-mean', { useNewUrlParser: true,})
+    .then(() => {
+        console.log('connected to db!');
+    }, (err) => {
+        console.log(err);
+    });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false, }));
@@ -15,8 +25,11 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-    const post = req.body;
-    console.log(post);
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content,
+    });
+    post.save();
     res.status(201).json({
         message: 'Post Added Sucessfully!'
     });
