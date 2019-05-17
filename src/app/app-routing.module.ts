216b54bffe70ef173f30fe8843020ba89e0a74a8
Amir Massarwa <amassarwi@gmail.com>
2019-05-17 22:22:28 +0200

    Added spa routing and header router links with some styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PostsListComponent,
+  },
+  {
+    path: 'create',
+    component: PostCreateComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
