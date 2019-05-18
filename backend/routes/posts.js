141bc90b21b91e634407673e1594806a80fba63b
Amir Massarwa <amassarwi@gmail.com>
2019-05-18 22:02:41 +0200

    Added separated file for posts routes -expressjs

---
+++ backend/routes/posts.js
@@ -0,0 +1,67 @@
+const exporess = require("express");
+
+const Post = require('../models/post');
+const router = exporess.Router();
+
+router.post('', (req, res, next) => {
+  const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+  });
+  post.save()
+  .then((createdPost) => {
+      res.status(201).json({
+          message: 'Post Added Sucessfully!',
+          postId: createdPost.id,
+      });
+  });
+});
+
+
+router.get('', (req, res, next) => {
+  Post.find()
+  .then((documents) => {
+      res.status(200).json({
+          message: 'Posts fetched successfully!',
+          posts: documents,
+      });
+  });
+});
+
+router.put('/:id', (req, res, next) => {
+  const post = new Post({
+      _id: req.body.id,
+      title: req.body.title,
+      content: req.body.content,
+  })
+  Post.updateOne({_id: req.params.id}, post)
+      .then(result => {
+          console.log(result);
+          res.status(200).json({message: 'Post updated!'});
+      });
+});
+
+router.get('/:id', (req, res, next) => {
+  Post.findById(req.params.id)
+      .then(post => {
+          if (post) {
+              res.status(200).json(post);
+          } else {
+              res.status(404).json({message: 'Post not found!'})
+          }
+      })
+});
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  Post.deleteOne({
+      _id: id,
+  })
+  .then((result) => {
+      res.status(200).json({
+          message: 'Post Deleted!'
+      });
+  });
+});
+
+module.exports = router;
